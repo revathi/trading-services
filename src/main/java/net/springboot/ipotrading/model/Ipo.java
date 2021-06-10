607a855f1f4106d67d9772eb5c4c8dca1bf27ae2
Revathi <revathi.balakrishnan.2010@gmail.com>
2021-06-10 13:34:46 +0530

    keep ipo and ipo Quote in same level

--- src/main/java/net/springboot/ipotrading/model/Ipo.java
+++ src/main/java/net/springboot/ipotrading/model/Ipo.java
@@ -1,18 +1,26 @@
 package net.springboot.ipotrading.model;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "ipo")
 public class Ipo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String userName;
     private String companyName;
     private double marketCap;
     private double availableForSale;
+    //ipo quote models
+    private String description;
+    private String issueDate;
+    private Long issueSize;
+    private double issuePrice;
+    private long lotSize;
+    private String closingDate;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     public Long getId() {
         return id;
     }
@@ -53,7 +61,65 @@ public class Ipo {
         this.availableForSale = availableForSale;
     }
 
+    public String getDescription() {
+        return description;
+    }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
+    public String getIssueDate() {
+        return issueDate;
+    }
+
+    public void setIssueDate(String issueDate) {
+        this.issueDate = issueDate;
+    }
+
+    public Long getIssueSize() {
+        return issueSize;
+    }
+
+    public void setIssueSize(Long issueSize) {
+        this.issueSize = issueSize;
+    }
+
+    public double getIssuePrice() {
+        return issuePrice;
+    }
+
+    public void setIssuePrice(double issuePrice) {
+        this.issuePrice = issuePrice;
+    }
+
+    public long getLotSize() {
+        return lotSize;
+    }
+
+    public void setLotSize(long lotSize) {
+        this.lotSize = lotSize;
+    }
+
+    public String getClosingDate() {
+        return closingDate;
+    }
+
+    public void setClosingDate(String closingDate) {
+        this.closingDate = closingDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ipo ipo = (Ipo) o;
+        return Double.compare(ipo.marketCap, marketCap) == 0 && Double.compare(ipo.availableForSale, availableForSale) == 0 && Double.compare(ipo.issuePrice, issuePrice) == 0 && lotSize == ipo.lotSize && Objects.equals(id, ipo.id) && Objects.equals(userName, ipo.userName) && Objects.equals(companyName, ipo.companyName) && Objects.equals(description, ipo.description) && Objects.equals(issueDate, ipo.issueDate) && Objects.equals(issueSize, ipo.issueSize) && Objects.equals(closingDate, ipo.closingDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userName, companyName, marketCap, availableForSale, description, issueDate, issueSize, issuePrice, lotSize, closingDate);
+    }
 
 }
